c62a8162a1b3782bcc8f7640d8c6fcb434d59233
juxqq <75832820+juxqq@users.noreply.github.com>
2022-12-25 15:36:14 +0100

    TD5

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,64 +1,24 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { AppMaterialModule } from './app-material.module';
-import { NavbarComponent } from './navbar/navbar.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatButtonModule } from '@angular/material/button';
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatIconModule } from '@angular/material/icon';
-import { MatListModule } from '@angular/material/list';
-import { LdapListComponent } from './ldap-list/ldap-list.component';
-import { MatTableModule } from '@angular/material/table';
-import { MatPaginatorModule } from '@angular/material/paginator';
-import { MatSortModule } from '@angular/material/sort';
-import {MatFormFieldModule} from "@angular/material/form-field";
-import {MatInputModule} from "@angular/material/input";
-import {MatSlideToggleModule} from "@angular/material/slide-toggle";
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {AppMaterialModule} from './app-material.module';
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import { LdapEditComponent } from './ldap-edit/ldap-edit.component';
-import { LdapAddComponent } from './ldap-add/ldap-add.component';
-import { LdapDetailComponent } from './share/ldap-detail/ldap-detail.component';
-import { AlertComponent } from './share/alert/alert.component';
+import {LdapManagementModule} from './ldap-management/ldap-management.module';
+import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    LdapListComponent,
-    LdapEditComponent,
-    LdapAddComponent,
-    LdapDetailComponent,
-    AlertComponent
+    PageNotFoundComponent,
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule,
-    AppMaterialModule,
-    LayoutModule,
-    MatToolbarModule,
-    MatButtonModule,
-    MatSidenavModule,
-    MatIconModule,
-    MatListModule,
-    MatTableModule,
-    MatPaginatorModule,
-    MatSortModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSlideToggleModule,
-    BrowserModule,
-    FormsModule,
-    ReactiveFormsModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    AppMaterialModule
+    BrowserModule, FormsModule, ReactiveFormsModule, BrowserAnimationsModule, AppMaterialModule, LdapManagementModule, AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
